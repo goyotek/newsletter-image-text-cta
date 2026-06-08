e7e41f68c1cf588dff57a66bd8edca9de5507640
goyotek <goyotek@gmail.com>
2026-06-08 12:22:51 +0200

    Add files via upload

--- img-txt/block.php
+++ img-txt/block.php
@@ -1,8 +1,8 @@
 <?php
 /*
- * Name: Image Text
+ * Name: Image Text CTA
  * Section: content
- * Description: An image and text block
+ * Description: A block with an image, title, text, and a CTA button in a single container
  */
 
 /* @var $options array */
@@ -10,27 +10,37 @@
 // On future releases of Newsletter, default options will be part of the options.php
 // file, it is the best place to have them. By now, be patience.
 
-// The "block_*" options are reserved and could be processed dutrectly by Newsletter. For example the
-// "block_background" and "block_padding_*" are used to generated the wrapper of the block content.
+// The "block_*" options are reserved and could be processed directly by Newsletter. For example the
+// "block_background" and "block_padding_*" are used to generate the wrapper of the block content.
 
 $default_options = array(
     'title' => 'Your stunning title',
     'text' => 'Your nice text to describe whatever you want to describe.',
+    'button_text' => 'Click Here',
+    'button_link' => '#',
+    'button_background' => '#b31e55',
+    'button_color' => '#f3f6f4',
+    'button_padding_vertical' => 10,
+    'button_padding_horizontal' => 25,
 
-    'block_padding_left' => 15,
-    'block_padding_right' => 15,
-    'block_padding_top' => 15,
+    'block_padding_left' => 0,
+    'block_padding_right' => 0,
+    'block_padding_top' => 0,
     'block_padding_bottom' => 15,
     'block_background' => '', // Leave empty to use the block background set on the newsletter settings
 );
 
 $options = array_merge($default_options, $options);
 
+// Generate title and text styles using TNP_Composer methods
+$title_style = TNP_Composer::get_title_style($options, 'title', $composer);
+$text_style = TNP_Composer::get_text_style($options, 'text', $composer);
+
 // Image preparation (again, that is a bit tricky...)
 
 $media = null;
 if (!empty($options['image']['id'])) {
-    // The $media is an onject containing the image URL and the size to specify in the HTML tag. The image is resized at
+    // The $media is an object containing the image URL and the size to specify in the HTML tag. The image is resized at
     // 2x to be sharp on mobile devices.
     $media = tnp_resize_2x($options['image']['id'], [$composer['width'], 0]);
     // Should never happen but... it happens
@@ -45,11 +55,16 @@ if (!empty($options['image']['id'])) {
 ?>
 
 <?php
-// Here we define a single block style with classes that are then transoformed in inline styles. Style for fonts can be easily generated with
+// Here we define a single block style with classes that are then transformed in inline styles. Style for fonts can be easily generated with
 // the object created above. The style block is REMOVED on rendering.
 ?>
 <style>
+    .title {
+        <?php $title_style->echo_css() ?>
+    }
+    
     .text {
+        <?php $text_style->echo_css() ?>
         padding: 10px 15px 10px 15px;
     }
 </style>
@@ -58,12 +73,37 @@ if (!empty($options['image']['id'])) {
 // The attribute "inline-class" is then replaced with a "style" attribute with all rules of the referenced class. 
 ?>
 
-<h1><?php echo $options['title']?></h1>
+<!-- Single container for the entire block -->
+<table role="presentation" border="0" cellpadding="0" cellspacing="0" align="center" width="100%" style="border-collapse: collapse; width: 100%; background-color: <?php echo esc_attr($options['block_background']); ?>; padding: <?php echo esc_attr($options['block_padding_top']); ?>px <?php echo esc_attr($options['block_padding_right']); ?>px <?php echo esc_attr($options['block_padding_bottom']); ?>px <?php echo esc_attr($options['block_padding_left']); ?>px;">
+    <tbody>
+        <tr>
+            <td align="center" style="padding: 0;">
+                <!-- Image -->
+                <?php if ($media) echo TNP_Composer::image($media) ?>
 
-<?php
-// This methos deal with CSS, attributes, link and so on. 
-?>
+                <!-- Title -->
+                <h2 inline-class="title" style="margin-bottom: 20px;"><?php echo $options['title'] ?></h2>
 
-<?php if ($media) echo TNP_Composer::image($media) ?>
+                <!-- Text -->
+                <p inline-class="text" style="margin-bottom: 20px;"><?php echo $options['text'] ?></p>
 
-<p inline-class="text"><?php echo $options['text']?></p>
+                <!-- Button -->
+                <?php if (!empty($options['button_text'])): ?>
+                    <table border="0" cellpadding="0" cellspacing="0" role="presentation" align="center" style="border-collapse: separate !important; line-height: 100%; width: auto; margin-top: 20px;">
+                        <tbody>
+                            <tr>
+                                <td align="center" bgcolor="<?php echo esc_attr($options['button_background']); ?>" role="presentation" style="border-collapse: separate !important; cursor: auto; mso-padding-alt: <?php echo esc_attr($options['button_padding_vertical']); ?>px <?php echo esc_attr($options['button_padding_horizontal']); ?>px; background: <?php echo esc_attr($options['button_background']); ?>; border-radius: 0px; border: 1px solid #bcbcbc;" valign="middle">
+                                    <a href="<?php echo esc_url($options['button_link']); ?>"
+                                       style="display: inline-block; color: <?php echo esc_attr($options['button_color']); ?>; font-family: <?php echo esc_attr($options['button_font_family'] ?? 'Lucida Sans Unicode, sans-serif'); ?>; font-size: <?php echo esc_attr(($options['button_font_size'] ?? '16') . 'px'); ?>; font-weight: <?php echo esc_attr($options['button_font_weight'] ?? 'normal'); ?>; line-height: 120%; margin: 0; text-decoration: none; text-transform: none; padding: <?php echo esc_attr($options['button_padding_vertical']); ?>px <?php echo esc_attr($options['button_padding_horizontal']); ?>px; mso-padding-alt: 0px; border-radius: 0px; width: auto;"
+                                       target="_blank">
+                                        <?php echo esc_html($options['button_text']); ?>
+                                    </a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                <?php endif; ?>
+            </td>
+        </tr>
+    </tbody>
+</table>
--- img-txt/options.php
+++ img-txt/options.php
@@ -8,11 +8,29 @@
  */
 ?>
 
+<?php $fields->media('image', 'Image') ?>
+
 <?php $fields->text('title', 'Title') ?>
+
+<?php $fields->font('title_font', 'Title font', ['family_default' => true, 'size_default' => true, 'weight_default' => true]) ?>
 
 <?php $fields->textarea('text', 'Text') ?>
 
-<?php $fields->media('image', 'Image') ?>
+<?php $fields->font('text_font', 'Text font', ['align' => true, 'family_default' => true, 'size_default' => true, 'weight_default' => true]) ?>
+
+<?php $fields->text('button_text', 'Button Text') ?>
+
+<?php $fields->url('button_link', 'Button Link') ?>
+
+<?php $fields->color('button_background', 'Button Background Color') ?>
+
+<?php $fields->color('button_color', 'Button Text Color') ?>
+
+<?php $fields->number('button_padding_vertical', 'Button Padding Vertical (px)', ['default' => 10]) ?>
+
+<?php $fields->number('button_padding_horizontal', 'Button Padding Horizontal (px)', ['default' => 25]) ?>
+
+<?php $fields->font('button_font', 'Button Font', ['family_default' => true, 'size_default' => true, 'weight_default' => true, 'color_default' => false]) ?>
 
 <?php 
 // Always add that at the bottom: it generates a set of options automatically processed by Newsletter 
